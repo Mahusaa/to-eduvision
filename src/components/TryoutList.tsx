8d125b5af4ceab1ead8e00d3acf2cd3a0350a9a5
Mahusaa <usamah.hafizh@ui.ac.id>
2025-01-17 13:58:52 +0700

    [feat] add leaderboard button

--- src/components/TryoutList.tsx
+++ src/components/TryoutList.tsx
@@ -3,8 +3,9 @@
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '~/components/ui/card';
 import { Button } from '~/components/ui/button';
 import { Badge } from '~/components/ui/badge';
-import { Trophy, Star, Clock, Users, Lock } from 'lucide-react';
+import { Trophy, Star, Clock, Users, Lock, BarChart } from 'lucide-react';
 import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 interface Tryout {
   id: number;
@@ -23,6 +24,18 @@ interface Tryout {
 interface TryoutListProps {
   tryoutData: Tryout[];
   userId: string;
+}
+const getStatusColor = (status: string) => {
+  switch (status) {
+    case 'open':
+      return 'bg-green-100 text-green-800'
+    case 'closed':
+      return 'bg-red-100 text-red-800'
+    case 'completed':
+      return 'bg-blue-100 text-blue-800'
+    default:
+      return 'bg-gray-100 text-gray-800'
+  }
 }
 
 export default function TryoutList({ tryoutData, userId }: TryoutListProps) {
@@ -97,52 +110,47 @@ export default function TryoutList({ tryoutData, userId }: TryoutListProps) {
               >
                 <CardHeader className="pb-3">
                   <div className="flex items-center justify-between">
-                    <CardTitle className="text-xl font-bold">
+                    <CardTitle className="text-xl font-bold text-primary">
                       {tryout.name} {tryout.tryoutNumber}
                     </CardTitle>
-                    <Badge
-                    >
-                      {tryout.status === 'closed'
-                        ? 'Closed'
-                        : tryout.status === 'completed'
-                          ? 'Completed'
-                          : 'Open'}
+                    <Badge className={`${getStatusColor(tryout.status)} font-semibold`}>
+                      {tryout.status.charAt(0).toUpperCase() + tryout.status.slice(1)}
                     </Badge>
                   </div>
                 </CardHeader>
 
-                <CardContent className="pb-2">
-                  <div className="flex items-center gap-4 text-sm text-muted-foreground">
+                <CardContent className="pb-2 justify-center">
+                  <div className="flex items-center gap-4 text-sm text-pretty">
                     <div className="flex items-center gap-1">
                       <Clock className="h-4 w-4" />
                       <span>{tryout.duration} menit</span>
                     </div>
-                    <div className="flex items-center">
-                      <Users className="w-4 h-4 mr-1" />
-                      100 participants
-                    </div>
                     {tryout.status === 'completed' && (
-                      <>
-                        <div className="flex items-center gap-1">
-                          <Star className="h-4 w-4" />
-                          <span>{tryout.score} points</span>
-                        </div>
-                        <div className="flex items-center gap-1">
-                          <Trophy className="h-4 w-4" />
-                          <span>Rank {tryout.rank}</span>
-                        </div>
-                      </>
+                      <div className="flex items-center gap-1">
+                        <Trophy className="h-4 w-4" />
+                        <span>Rank {tryout.rank}</span>
+                      </div>
                     )}
                   </div>
 
-                  <div className="mt-4 text-sm text-muted-foreground">
-                    Berakhir pada: {new Date(tryout.endedAt).toLocaleDateString()}
+                  <div className="mt-4 text-sm text-muted-foreground text-center">
+                    Berakhir pada: {new Date(tryout.endedAt).toLocaleDateString()} pukul 00:00 WIB
                   </div>
                 </CardContent>
 
-                <CardFooter className="pt-4 mt-6">
+                <CardFooter className=" flex flex-col gap-3">
+                  {tryout.status === 'completed' && (
+                    <Link href={`/leaderboard/${tryout.id}`} className='w-full mt-2'>
+                      <Button
+                        className="w-full"
+                        variant="outline"
+                      >
+                        <BarChart className="w-4 h-4 mr-2" /> Lihat Leaderboard
+                      </Button>
+                    </Link>
+                  )}
                   <Button
-                    className="w-full"
+                    className={`w-full ${tryout.status === 'completed' ? '' : 'mt-16'}`}
                     variant={
                       tryout.status === 'open'
                         ? isTryoutEnded
@@ -155,7 +163,7 @@ export default function TryoutList({ tryoutData, userId }: TryoutListProps) {
                         ? handleStart(tryout)
                         : alert('Pembahasan Belum tersedia')
                     }
-                    disabled={tryout.status === 'closed'}
+                    disabled={tryout.status === 'closed' || tryout.status === 'completed'}
                   >
                     {tryout.status === 'open'
                       ? isTryoutEnded
@@ -165,6 +173,7 @@ export default function TryoutList({ tryoutData, userId }: TryoutListProps) {
                         ? (<><Lock className="w-4 h-4" /> Lihat Pembahasan </>)
                         : 'Belum Tersedia'}
                   </Button>
+
                 </CardFooter>
               </Card>
             );
